a9a51d5740a07f8360e0c2d734287cb4b35963b2
fatiger <fatiger92@gmail.com>
2022-09-14 10:57:38 +0900

    Commit

---
+++ Study/FirstWinger/Assets/Scripts/FWNetworkManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+// 네트워크는 다른 걸 이용해서 해야함.
+// PackageManager 에서 MultiPlayer HLAPI 를 받아서 사용하라는데 2019년도에 나온 삭제된 기능임.
+// 장난까세요?
+
+public class FWNetworkManager
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
