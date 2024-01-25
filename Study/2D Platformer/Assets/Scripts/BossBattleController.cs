8941b26f807b2341cc0186e404d61cfd72f40be0
jiyoungjun <72366776+fatiger92@users.noreply.github.com>
2024-01-25 17:59:29 +0900

    c
    c

--- Study/2D Platformer/Assets/Scripts/BossBattleController.cs
+++ Study/2D Platformer/Assets/Scripts/BossBattleController.cs
@@ -5,19 +5,67 @@ using UnityEngine;
 public class BossBattleController : MonoBehaviour
 {
     bool bossActive;
+
+    public GameObject blockers;
+    public Transform camPoint;
+    CameraController camController;
+
+    public float cameraMoveSpeed;
+
+    public Transform theBoss;
+    public float bossGrowSpeed = 2f;
+
+    public Transform projectileLauncher;
+    public float launcherGrowSpeed = 2f;
+
+    public float launcherRotateSpeed = 90f;
+    float launcherRotation;
     
     void Start()
     {
-        
+        camController = FindFirstObjectByType<CameraController>();
+
+        theBoss.localScale = Vector3.zero;
     }
 
     void Update()
     {
-        
+        if (bossActive == true)
+        {
+            camController.transform.position = Vector3.MoveTowards(
+                camController.transform.position,
+                camPoint.position,
+                cameraMoveSpeed * Time.deltaTime);
+
+            if (theBoss.localScale != Vector3.one)
+            {
+                theBoss.localScale = Vector3.MoveTowards(
+                    theBoss.localScale,
+                    Vector3.one,
+                    bossGrowSpeed * Time.deltaTime);
+            }
+
+            if (projectileLauncher.transform.localScale != Vector3.one)
+            {
+                projectileLauncher.localScale = Vector3.MoveTowards(
+                    projectileLauncher.localScale,
+                    Vector3.one,
+                    launcherGrowSpeed * Time.deltaTime);
+            }
+
+            launcherRotation += launcherRotateSpeed * Time.deltaTime;
+
+            if (launcherRotation > 360f)
+                launcherRotation -= 360f;
+
+            projectileLauncher.transform.localRotation = Quaternion.Euler(0f, 0f,launcherRotation);
+        }
     }
 
     public void ActivateBattle()
     {
         bossActive = true;
+        blockers.SetActive(true);
+        camController.enabled = false;
     }
 }
---
+++ Study/2D Platformer/Assets/Scripts/BossProjectile.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossProjectile : MonoBehaviour
+{
+    public float speed = 8f;
+    Vector3 direction;
+
+    public float lifetime = 3f;
+    void Start()
+    {
+        direction = (PlayerHealthController.instance.transform.position - transform.position).normalized;
+        
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+        transform.position += direction * Time.deltaTime * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerHealthController.instance.DamagePlayer();
+            Destroy(gameObject);
+        }
+    }
+}
